e0afb68eaa006513456fa02c624adf8291d327fc
Chen Lixiang <lixiang3608@outlook.com>
2019-02-17 17:51:20 +0800

    Add grade page

--- lib/views/pages/grade.dart
+++ lib/views/pages/grade.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cugapp/utils/global_config.dart';
 
 class GradePageWidget extends StatefulWidget {
   @override
@@ -7,24 +8,49 @@ class GradePageWidget extends StatefulWidget {
 
 class GradePageWidgetState extends State<GradePageWidget> {
 
+  var dropdownValue = "2018年春季学期";
+
+  void freshGrade() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: '我的成绩',
       home: Scaffold(
         appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios), 
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
           title: Text('我的成绩'),
-          backgroundColor: Colors.blue[400],
         ),
         body: Container(
           child: Column(
             children: <Widget>[
               ListTile(
-                title: Text("选择学期!"),
-              ),
-              Divider(
-                color: Colors.black87,
-                height: 0,
+                title: const Text('选择学期'),
+                trailing: DropdownButton<String>(
+                  value: dropdownValue,
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownValue = newValue;
+                    });
+                  },
+                  items: <String>[
+                      '2019年春季学期', '2018年秋季学期', '2018年春季学期'
+                    ]
+                    .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    })
+                    .toList(),
+                ),
               ),
             ],
           )
