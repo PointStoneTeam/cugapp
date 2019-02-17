9c601d4d9bfed90c4bda9c17d1131d889c5d3ba8
Chen Lixiang <lixiang3608@outlook.com>
2019-02-17 17:51:05 +0800

    add util

---
+++ lib/utils/global_config.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GlobalConfig {
+
+  static ThemeData themeData = new ThemeData.dark();
+
+  // Color
+  static var topicBlue = Colors.blue[500];
+
+  static var dividerBlack = Colors.black87;
+
+  static var dividerGrey = Colors.black45;
+
+  static var backgroundColor = Colors.grey[200];
+}
